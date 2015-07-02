cca97aa9dae43eb194e9e795df8c9f639928e20c
Bill Fogg <BillFogg@Bills-MacBook-Pro.local>
2015-07-02 12:34:30 -0400

    added devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :books
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  root 'books#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
